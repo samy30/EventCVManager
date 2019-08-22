6a5f4742e8adf145fe2b03faf62b68c9b6cfcb55
samy30 <samibelaid30@gmail.com>
2019-08-22 15:52:08 +0100

    add security

--- Backend/src/main/java/com/example/Backend/Controller/CVController.java
+++ Backend/src/main/java/com/example/Backend/Controller/CVController.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
 public class CVController {
     @Autowired
     private CVRepository cvRepository;
---
+++ Backend/src/main/java/com/example/Backend/Model/RoleName.java
@@ -0,0 +1,6 @@
+package com.example.Backend.Model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
