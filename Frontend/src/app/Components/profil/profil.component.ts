c1d508efba7038b12f72985ead1bfc13c4bcbc0f
samy30 <samibelaid30@gmail.com>
2019-09-10 22:37:47 +0100

    add enterprise update form

--- Frontend/src/app/Components/profil/profil.component.ts
+++ Frontend/src/app/Components/profil/profil.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Message } from 'primeng/components/common/message';
 import { MessageService } from 'primeng/components/common/messageservice';
-import {UserService} from "../../Services/user.service";
+import {UserService} from '../../Services/user.service';
 
 
 @Component({
@@ -14,89 +14,123 @@ import {UserService} from "../../Services/user.service";
 })
 export class ProfilComponent implements OnInit {
   userFormGroup: FormGroup;
-  user:any;
-  Uploading = false;
-  Uploaded = true;
+  enterpriseFormGroup: FormGroup;
+  user: any;
   image;
   msgs: Message[] = [];
-  uploadedFiles: any[] = [];
-  imgURL:any=null;
-  value: number = 0;
+  imgURL: any = null;
+  value = 0;
+  role: null;
   constructor(
     private userService: UserService,
     private cd: ChangeDetectorRef,
-    private formBuilder: FormBuilder,
-    private messageService: MessageService) {
-      this.userFormGroup = this.formBuilder.group({
-        username: ['', Validators.required],
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required],
-        email: ['', [ Validators.required, Validators.email]],
-        age: ['',  Validators.required],
-        gender: ['',  Validators.required]
-     });
+    private formBuilder: FormBuilder) {
+    this.userFormGroup = this.formBuilder.group({
+      username: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [ Validators.required, Validators.email]],
+      age: ['',  Validators.required],
+      gender: ['',  Validators.required],
+      town: ['',  Validators.required],
+    });
+    this.enterpriseFormGroup = this.formBuilder.group({
+      username: ['', Validators.required],
+      name: ['', Validators.required],
+      email: ['', [ Validators.required, Validators.email]],
+      description: ['',  Validators.required],
+      activity: ['',  Validators.required]
+    });
 
-     }
+  }
 
   ngOnInit() {
-
     this.loadUser();
-
-
   }
-  loadUser()
-  {
+  loadUser() {
     this.user = JSON.parse(localStorage.getItem('currentUser'));
-      this.userFormGroup.patchValue({
-        username : this.user.username,
-        email : this.user.email,
-        firstName : this.user.firstName,
-        lastName : this.user.lastName,
-        age:this.user.age,
-        gender:this.user.gender
-      })
-      this.image=this.user.image;
-      console.log("profile image");
-      console.log(this.image);
+    this.userFormGroup.patchValue({
+      username : this.user.username,
+      email : this.user.email,
+      firstName : this.user.firstName,
+      lastName : this.user.lastName,
+      age: this.user.age,
+      gender: this.user.gender,
+      town: this.user.town
+    });
+    this.enterpriseFormGroup.patchValue({
+      name : this.user.name,
+      username: this.user.username,
+      email: this.user.email,
+      description: this.user.description,
+      activity: this.user.activity
+    });
+    this.image = this.user.image;
+    console.log('profile image');
+    console.log(this.image);
+    this.role = this.user.authorities[0].authority;
   }
   updateUser() {
-    let {username,email,firstName,lastName,age,gender} = this.userFormGroup.value;
-    let data = {
+    const {username, email, firstName, lastName, age, gender} = this.userFormGroup.value;
+    const data = {
       username,
       email,
       firstName,
       lastName,
       age,
       gender,
-      image:this.imgURL
+      image: this.imgURL
     };
-     console.log("hello");
-      console.log(data);
-     var id=this.user.id;
+    console.log('hello');
+    console.log(data);
+    const id = this.user.id;
     this.userService.updateUser(id, data).subscribe(user => {
-      this.showSuccess('Votre profil a été mis a jour','Profil Modifie' ,'success' );
+      this.showSuccess('Votre profil a été mis a jour', 'Profil Modifie' , 'success' );
       console.log(user);
-    //  this.
+      //  this.
       this.loadUser();
 
- },err =>{
-  this.showSuccess('Erreur ',err.error.message,'error')
- });
-}
-showSuccess( title , message, type) {
-  this.msgs = [];
-  this.msgs.push({severity: type, summary: title, detail: message });
-  setTimeout(() => {
-  this.msgs = [];
+    }, err => {
+      this.showSuccess('Erreur ', err.error.message, 'error');
+    });
+  }
+
+  updateEnterprise() {
+    const {name, username, email, description, activity} = this.enterpriseFormGroup.value;
+    const data = {
+      username,
+      email,
+      name,
+      description,
+      activity,
+      image: this.imgURL
+    };
+    console.log('hello');
+    console.log(data);
+    const id = this.user.id;
+    this.userService.updateUser(id, data).subscribe(user => {
+      this.showSuccess('Votre profil a été mis a jour', 'Profil Modifie' , 'success' );
+      console.log(user);
+      //  this.
+      this.loadUser();
+
+    }, err => {
+      this.showSuccess('Erreur ', err.error.message, 'error');
+    });
+  }
+
+  showSuccess( title , message, type) {
+    this.msgs = [];
+    this.msgs.push({severity: type, summary: title, detail: message });
+    setTimeout(() => {
+      this.msgs = [];
     }, 2000);
+  }
+
+  getImage(imgURL) {
+    this.imgURL = imgURL;
+  }
+
 }
-
-getImage(imgURL){
- this.imgURL=imgURL;
-}
-
-}
-
-
 
 
